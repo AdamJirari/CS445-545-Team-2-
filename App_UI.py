b83da971188552803f46fbcb85177f52b5d3f0af
MertAydin7 <89756689+MertAydin7@users.noreply.github.com>
2023-08-01 21:17:58 +0300

    Update App_UI.py
    Add two buttons for users to get interact via click. Add functions to display the corresponding results such as most tracked songs, and artists.

--- App_UI.py
+++ App_UI.py
@@ -2,15 +2,62 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy
 
-class WrapperApp(App):
+class SpotifyWrapperApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # Spotify API authentication
+        scope = "user-top-read"
+        sp_oauth = SpotifyOAuth(client_id='YOUR_CLIENT_ID',
+                                client_secret='YOUR_CLIENT_SECRET',
+                                redirect_uri='YOUR_REDIRECT_URI',
+                                scope=scope)
+        token_info = sp_oauth.get_cached_token()
+        if not token_info:
+            auth_url = sp_oauth.get_authorize_url()
+            print("Please visit this URL to authenticate:", auth_url)
+            auth_response = input("Enter the URL you were redirected to: ")
+            token_info = sp_oauth.get_access_token(auth_response)
+        self.spotify = spotipy.Spotify(auth=token_info['access_token'])
+
+    def get_most_tracked_artists(self):
+        results = self.spotify.current_user_top_artists(limit=10, time_range='medium_term')
+        return results['items']
+
+    def get_most_tracked_songs(self):
+        results = self.spotify.current_user_top_tracks(limit=10, time_range='medium_term')
+        return results['items']
+
     def build(self):
         layout = BoxLayout(orientation='vertical')
-        label = Label(text='Spotify Wrapper!')
-        button = Button(text='test')
 
-        layout.add_widget(label)
-        layout.add_widget(button)
+        button_layout = BoxLayout(orientation='horizontal')
+        most_tracked_artists_button = Button(text='Most Tracked Artists', on_press=self.show_most_tracked_artists)
+        most_tracked_songs_button = Button(text='Most Tracked Songs', on_press=self.show_most_tracked_songs)
+        button_layout.add_widget(most_tracked_artists_button)
+        button_layout.add_widget(most_tracked_songs_button)
+
+        self.result_label = Label(text='')
+        layout.add_widget(button_layout)
+        layout.add_widget(self.result_label)
 
         return layout
-WrapperApp.run()
+
+    def show_most_tracked_artists(self, instance):
+        artists = self.get_most_tracked_artists()
+        self.show_results(artists)
+
+    def show_most_tracked_songs(self, instance):
+        songs = self.get_most_tracked_songs()
+        self.show_results(songs)
+
+    def show_results(self, items):
+        result_text = ""
+        for item in items:
+            result_text += f"{item['name']} - {item['artists'][0]['name']}\n"
+        self.result_label.text = result_text
+
+SpotifyWrapperApp().run()
